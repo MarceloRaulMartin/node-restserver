36c6a668005b85a9b541b044a0cb9b67203342f3
Marcelo Martin <marceloraulmartin@gmail.com>
2023-11-21 18:42:33 -0300

    Fin seccion 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const {response} = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
     const {correo, password} = req.body;
@@ -52,6 +53,49 @@ const login = async (req, res = response) => {
             });
     }
 }
+
+const googleSignIn = async( req, res = response) => {
+    const { id_token } = req.body;
+    try {
+        const { correo, nombre, img} = await googleVerify( id_token); // Extraigo las variables que necesito
+        let usuario = await Usuario.findOne({correo}); //Verifico en BD si existe correo
+        if (!usuario){          // Resultado de findOne el usuario no existe
+            //Tengo que crearlo 
+            const data = {      // Preparo la data
+                nombre,         // Uso el nombre de googleVerify
+                correo,         // Uso el correo de googleVerify
+                password: ':P',   // Asigno un pasword generico porque me obliga la BD
+                img,            // Uso la img de googleVerify
+                rol: 'USER_ROLE',
+                google: true    
+            };
+            usuario = new Usuario(data); //Creo nuevo usuario
+            await usuario.save();        //Grabo usuario en BD
+
+        }
+        //Validaciones del usuario encontrado
+        if (!usuario.estado) {                  // Usuario Bloqueado
+            return res.status(401).json({
+                msg: 'Hable con el administrador usuario bloqueado'
+            });
+        };
+        // Generar el JsonWebToken JWT
+        const token = await generarJWT(usuario.id)
+    
+        res.json ({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de google es invalido'
+        })
+    }
+
+}
+
 module.exports ={
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator'); //Inserta la funcion check desde validator
-const {login}   = require('../controllers/auth');  // Controlador de auth
+const {login, googleSignIn}   = require('../controllers/auth');  // Controlador de auth
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -9,5 +9,10 @@ router.post('/login',[
     check('password','La contrasena es obligatoria').not().isEmpty(),
     validarCampos
 ], login);    // llamo mi controller/auth que me devuelve el login
+
+router.post('/google',[
+    check('id_token','id_token de Google es necesario').not().isEmpty(),
+    validarCampos
+], googleSignIn);
 
 module.exports = router;
